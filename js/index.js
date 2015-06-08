a5daef0cc8dc4b400031c700191aae7846343403
Anna Lezhikova <anna.v.l@gmail.com>
2015-06-09 10:01:12 +1200

    add at index show on click

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('body').on('click', function() {
+		$('.verticalImage').show();
+		$('h2').text('Choose a door to your destiny.');
+		$('h3').text(' ') 			
+	});
+});
