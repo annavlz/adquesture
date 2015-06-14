4c36c0c4b8b396c3fd22480c4dd683de22b96c5b
Anna Lezhikova <anna.v.l@gmail.com>
2015-06-14 21:07:34 +1200

    add talk box to well

--- js/well.js
+++ js/well.js
@@ -1,90 +1,67 @@
 // Opens dialog box on click or key pressed.
-
 $(document).ready(function() {
 	$('html').on('click', function() {
 		$('form').show();
 		$('.answer').show();
-	
+		// $('h2').text('Click the button, do not press Enter')
 
-	});
-	$('html').on('keydown', function(e) {
-		$('form').show();
-		$('.answer').show();
-			
 	});
 });
 
-// Number of tries to talk before game is over.
 var count = 0;
 
+console.log(count);
 
-// Gets data from users input and returns answer.
 function testResults (form) {
-    var visitorAnswer = form.inputbox.value;
-    var wordList = visitorAnswer.toLowerCase().split(" ");
+    visitorAnswer = form.inputbox.value;
     
-    // Searches the user's answer for matches.
-    function inTheList(word, list) {
-    	for (i in list) {
-    		if (list[i] === word) {
-    			return true;
-    		} 
-    	}	
-    	return false;
+    // possible answers
+    var responses = {
+    	"hi": "Talking to yourself? Already?",
+    	"hello": "Talking to yourself? Already?",
+    	"hey": "Talking to yourself? Already?",
+    	"ahoj": "Talking to yourself? Already?",
+    	"greetings": "Talking to yourself? Already?",
+    	"out": "The walls are too steep.",
+    	"shout": "Someone is outside. Do you want help?"
+    	
+
     };
 
-    // Checks if the answer is empty.
-    function emptyList(list) {
-    	for (i in list) {
-    		if (list[i].length != 0) {
-    			return true;
-    		}
-    	}
-    	return false;
-    }
-
-    // Answers.
-
-   	if (!emptyList(wordList)) {
-		$('h2').text("Doing nothing can help.")
-		$('h3').text("But not in your case.")
-		count += 1;
-	}
-
-
-	else if (inTheList("yes", wordList)
-		|| inTheList("yeah", wordList)
-		|| inTheList("y", wordList)) {
-
-		window.location="pass_out.html";
-	}
-
-	else if (inTheList("shout", wordList)
-		|| inTheList("cry", wordList)) {
-		$('h2').text("There is someone there!");
-		$('h3').text("He asks if you need help.");
 	
-	} else {
-		$('h2').text("I don't understand.")
-		$('h3').text("")
-
-		count += 1;
-		
-		if (count == 5) {
-			$('h2').text('Where are you?')
-			$('h3').text('May be there is someone around')
-			
+	// gives answers
+	for(var key in responses) {
+		if (visitorAnswer.search(key) >= 0){
+			$('h2').text(responses[key]);
+			break;
+		} else if (visitorAnswer.search("yes") >= 0
+			|| visitorAnswer.search("yeah") >= 0
+			|| visitorAnswer.search("yep") >= 0) {
+			window.location="pass_out.html";
+		} else if (visitorAnswer.length == 0) {
+			$('h2').text("Doing nothing won't help in this situation");
+			break;
+		}else{$('h2').text("I don't understand you.");
 		}
-
-		if (count > 10) {
-			$('.answer').remove();
+	};
+	count++;
+	// number of tries
+	if (count == 5) {
+			$('h2').text('Is there anyone outside?');
+			$('h3').text('');
+			
+		};
+	if (count == 10) {$('h2').text("You are tired.")};
+	if (count > 10) {
+			
 			$('form').remove();
-			$('h2').text("You have died of dehydration and cold.");
+			$('h2').text("You've died of cold.");
 			$('h3').text("Sad...");
 		};
-	}
-	// Clears the input box.
+	console.log(count);
+    // Clears the input box."Pardon? My handsome tasty dinner."
 	form.inputbox.value = "";
-	
-};
+ };
+
+
 
